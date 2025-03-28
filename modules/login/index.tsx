c4aa5bba05aff43845632ede3f992f715e96ebf4
MicksCoder <mickaelgoliveira@gmail.com>
2025-03-28 17:32:19 -0300

    criando modulo de login

---
+++ modules/login/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "@/components/Themed"
+
+const Login = () => {
+    return (
+        <View>
+            <Text>
+                AAAAAAAAAAA
+            </Text>
+        </View>
+    )
+}
+
+export default Login
